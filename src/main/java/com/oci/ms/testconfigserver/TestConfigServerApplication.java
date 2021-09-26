3134c55dfae0d47413eeeb0c447bb739e769d5ca
Karlos Chita <karloslcp@gmail.com>
2021-09-26 16:35:28 -0500

    Initial commit

---
+++ src/main/java/com/oci/ms/testconfigserver/TestConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.oci.ms.testconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestConfigServerApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(TestConfigServerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/oci/ms/testconfigserver/TestConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oci.ms.testconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestConfigServerApplicationTests
+{
+    @Test
+    void contextLoads()
+    {
+    }
+}
